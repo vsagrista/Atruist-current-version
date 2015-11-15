a280e4be6c1a2b104032f0c6ebccc9fec9251fe6
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-11-15 18:03:56 +0100

    Added edit and update page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,7 +23,21 @@ include UsersHelper
     @user = current_user  
   end
 
+  def edit 
+     @user = current_user  
+  end 
+
   def update
-    #binding.pry
+    if current_user.update(user_params)
+      flash[:notice] = "Update completed!"
+    else 
+      flash[:notice] = "Something went wrong!"
+    end
+    redirect_to '/users/dashboard'
+  end
+
+  private 
+  def user_params
+    params.require(:user).permit(:name,:email,:password,:address, :about)
   end
 end
