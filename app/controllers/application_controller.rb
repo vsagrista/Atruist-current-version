3b6139a9fce6e7791b8c4297a29275d8f31fb8ce
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-11-15 22:04:02 +0100

    Made new action and view for talents

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :configure_permitted_parameters, :only => [:create]
-
+  #before_filter :configure_permitted_parameters, :only => [:create]
+  before_action :configure_permitted_parameters, :if => :devise_controller?, :only => [:create]
   def after_sign_in_path_for(user)
          users_dashboard_path
   end
-
 
   protected
 
--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -5,8 +5,28 @@ class TalentsController < ApplicationController
 	end
 	def show
 	end
+	def new
+		@user = current_user
+		@talent = Talent.new
+	end
 	def edit
 	end
-	def update
+
+	def create
+		talent = Talent.new(talent_params)
+		talent.update(user_id: current_user.id)
+		if talent.save
+      flash[:notice] = "Talent saved!"
+    else 
+      flash[:notice] = talent.errors.full_messages.to_sentence
+    end
+    redirect_to user_talents_path(current_user)
 	end
+	def update
+
+	end
+	private 
+	  def talent_params
+	    params.require(:talent).permit(:name,:description)
+	  end
 end
