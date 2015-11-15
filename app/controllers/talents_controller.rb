6ab872511a7ede821ade16e693ca8d71c3f65107
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-11-15 22:44:47 +0100

    Made update action and view

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -3,13 +3,17 @@ class TalentsController < ApplicationController
 		@user = current_user
 		@talents = @user.talents
 	end
+	
 	def show
 	end
+
 	def new
 		@user = current_user
 		@talent = Talent.new
 	end
 	def edit
+		@user = current_user
+		@talent = Talent.find(params[:id])
 	end
 
 	def create
@@ -22,9 +26,17 @@ class TalentsController < ApplicationController
     end
     redirect_to user_talents_path(current_user)
 	end
-	def update
 
+	def update
+		talent = Talent.find(params[:id])
+		if talent.update(talent_params)
+			flash[:notice] = "Talent saved!"
+		else 
+			flash[:notice] = talent.errors.full_messages.to_sentence
+		end
+		redirect_to user_talents_path(current_user)
 	end
+
 	private 
 	  def talent_params
 	    params.require(:talent).permit(:name,:description)
