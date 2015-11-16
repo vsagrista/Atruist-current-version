41f87fa7c012ebd1d49f391c2398ee4d6761198b
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-11-16 22:01:51 +0100

    Added logic to yaml file in model, gets added to list if new

--- app/controllers/talents_controller.rb
+++ app/controllers/talents_controller.rb
@@ -18,21 +18,15 @@ class TalentsController < ApplicationController
 	end
 
 	def create
-		talent_choices = YAML::load_file("#{Rails.root}/config/talent_choices.yml") #Load
-		talent_choices["profession"] << "casa"
- 		# d["profession"].include? "casa" Method to add to yaml
-
-
-
-
 		talent = Talent.new(talent_params)
+		talent.add_talent_if_not_on_yaml_list(params[:talent][:name])
 		talent.user_id = current_user.id
 		if talent.save
       flash[:notice] = "Talent saved!"
     else 
       flash[:notice] = talent.errors.full_messages.to_sentence
     end
-    redirect_to user_talents_path(current_user)
+      redirect_to user_talents_path(current_user)
 	end
 
 	def update
--- app/models/talent.rb
+++ app/models/talent.rb
@@ -5,4 +5,14 @@ class Talent < ActiveRecord::Base
   	# validates_inclusion_of :sport, in: Talent_choices['sport']
   	# validates_inclusion_of :activity, in: Talent_choices['activity']
 
+  	def add_talent_if_not_on_yaml_list(talent)
+  		talent_choices = YAML::load_file("#{Rails.root}/config/talent_choices.yml") #Load
+		unless talent_choices["profession"].include? talent
+			talent_choices["profession"] << talent
+ 		end
+ 		File.open("#{Rails.root}/config/talent_choices.yml", 'w') do |fo|
+		  fo.puts talent_choices.to_yaml
+		end
+	end
+
 end
