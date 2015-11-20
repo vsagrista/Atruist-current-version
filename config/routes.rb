1fdce9bc41cc401a55a0b4bd587f3e354bfdf56c
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-11-20 16:18:18 +0100

    Changed api, stable

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,15 @@ devise_for :users
 
 get "/users/dashboard" => "users#dashboard"
 get "/users/address" => "users#address"
+get "/users/search" => "talents#search"
+
+namespace :api  do
+  namespace :v1 do
+  	get "/addresses" => "base#addresses"
+  	get "/addresses/cities" => "base#cities"
+    resources :base, only: [:index, :show]
+  end
+end
 
 authenticated :user do
   root :to => "users#redirect_to_dashboard", as: :authenticated_root
