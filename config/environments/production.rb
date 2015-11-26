134d18762db00f37812d53efa8e4daa3b4b0e6cd
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-11-26 14:46:41 +0100

    Changed mailer config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,7 +23,7 @@
   address: "smtp.gmail.com",
   port: 587,
   domain: "gmail.com",
-  authentication: :login,
+  authentication: "plain",
   enable_starttls_auto: true,
   user_name: ENV["GMAIL_USERNAME"],
   password: ENV["GMAIL_PASSWORD"]
