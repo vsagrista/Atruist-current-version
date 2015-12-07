446d1c5e0d0b7b4a93a81ef048b4ebd864094354
Victor Sagrista Lopez <victorsagristalopez@gmail.com>
2015-12-07 22:21:17 +0100

    Seeded db using faker gem

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source 'https://rubygems.org'
 
+gem 'faker'
+
 gem 'rails_12factor'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '4.2.3'
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,4 +6,14 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
+talents = ["Cooking", "English Lessons", "Paining", "Math Lessons", "Coding Lessons"]
+5.times do 
+	User.create(
+		name: Faker::Name.name, 
+		email: Faker::Internet.email, 
+		password: "12345678",
+		address: "#{Faker::Address.street_name}, #{Faker::Address.city}, #{Faker::Address.country}")
+	Talent.create(name: talents.sample, description: Faker::Lorem.sentence, city: User.last.address.split(",")[1].split(" ")[0])
+end
+
 
